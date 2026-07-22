95a9634c64871529f176657ce832a8f19af2efd5
Tim Hankinson <hammerhankinson@gmail.com>
2026-07-22 14:29:34 -0400

    Initial scaffolding and setup

---
+++ src/data/products.js
@@ -0,0 +1,122 @@
+export const products = [
+  {
+    id: 'professional',
+    name: 'Professional',
+    subtitle: 'Black Cabrio',
+    price: 105,
+    badges: ['Best Seller'],
+    variants: [
+      {
+        id: 'black',
+        color: 'Black',
+        swatch: '#242424',
+        images: [
+          {
+            src: '/images/products/professional-black-1.png',
+            alt: 'Professional black clog, right-facing view',
+          },
+          {
+            src: '/images/products/professional-black-2.png',
+            alt: 'Professional black clog, left-facing view',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 'merrin',
+    name: 'Merrin',
+    subtitle: 'Black Cabrio',
+    price: 155,
+    badges: ['New', 'Best Seller'],
+    variants: [
+      {
+        id: 'black',
+        color: 'Black',
+        swatch: '#242424',
+        images: [
+          {
+            src: '/images/products/professional-black-1.png',
+            alt: 'Merrin black clog, right-facing view',
+          },
+          {
+            src: '/images/products/professional-black-2.png',
+            alt: 'Merrin black clog, left-facing view',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 'rowan',
+    name: 'Rowan',
+    subtitle: 'Black Cabrio',
+    price: 150,
+    badges: [],
+    variants: [
+      {
+        id: 'black',
+        color: 'Black',
+        swatch: '#242424',
+        images: [
+          {
+            src: '/images/products/professional-black-1.png',
+            alt: 'Rowan black clog, right-facing view',
+          },
+          {
+            src: '/images/products/professional-black-2.png',
+            alt: 'Rowan black clog, left-facing view',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 'brenna',
+    name: 'Brenna',
+    subtitle: 'Black Cabrio',
+    price: 110,
+    badges: [],
+    variants: [
+      {
+        id: 'black',
+        color: 'Black',
+        swatch: '#242424',
+        images: [
+          {
+            src: '/images/products/professional-black-1.png',
+            alt: 'Brenna black clog, right-facing view',
+          },
+          {
+            src: '/images/products/professional-black-2.png',
+            alt: 'Brenna black clog, left-facing view',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 'fawna',
+    name: 'Fawna',
+    subtitle: 'Black Cabrio',
+    price: 110,
+    badges: [],
+    variants: [
+      {
+        id: 'black',
+        color: 'Black',
+        swatch: '#242424',
+        images: [
+          {
+            src: '/images/products/professional-black-1.png',
+            alt: 'Fawna black clog, right-facing view',
+          },
+          {
+            src: '/images/products/professional-black-2.png',
+            alt: 'Fawna black clog, left-facing view',
+          },
+        ],
+      },
+    ],
+  },
+]
